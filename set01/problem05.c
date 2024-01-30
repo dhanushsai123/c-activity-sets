07db7d62ca392f880850558ed7236cfa39280c53
Boya shalini <boyas.bsc23@rvu.edu.in>
2024-01-30 10:25:10 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+int input() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    return num;
+}
+
+int compare(int a, int b, int c) {
+    int largest = a;
+    
+    if (b > largest) {
+        largest = b;
+    }
+    if (c > largest) {
+        largest = c;
+    }
+    
+    return largest;
+}
+
+void output(int a, int b, int c, int largest) {
+    printf("The largest of %d, %d, and %d is %d.\n", a, b, c, largest);
+
+}
+
+int main() {
+    int num1, num2, num3, largest;
+
+    num1 = input();
+    num2 = input();
+    num3 = input();
+
+    largest = compare(num1, num2, num3);
+
+    output(num1, num2, num3, largest);
+
+    return 0;
+}
