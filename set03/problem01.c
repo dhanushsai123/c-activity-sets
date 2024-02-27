b7802949c2e066ad4bdca7c559a011e622827f34
Boya shalini <boyas.bsc23@rvu.edu.in>
2024-02-27 14:37:40 +0530

    test

--- set03/problem01.c
+++ set03/problem01.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+
+void input(float *x1, float *y1, float *x2, float *y2);
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
 
 int main() {
-    float x1, y1, x2, y2, gdistance; // Declare variables for coordinates and distance
-    
-    // coordinates (x1, y1) and store them
-    printf("Input x1: ");
-    scanf("%f", &x1);
-    printf("Input y1: ");
-    scanf("%f", &y1);
-    
-    // coordinates (x2, y2) and store them
-    printf("Input x2: ");
-    scanf("%f", &x2);
-    printf("Input y2: ");
-    scanf("%f", &y2);
-    
-    // Calculate squared distance between points
-    gdistance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
-    
-    // Calculate and print the distance between the points
-    printf("Distance between the said points: %.4f", sqrt(gdistance));
-    printf("\n");
-    
+    float x1, y1, x2, y2;
+    input(&x1, &y1, &x2, &y2);
+
+    float distance = find_distance(x1, y1, x2, y2);
+
+    output(x1, y1, x2, y2, distance);
+
     return 0;
+}
+
+void input(float *x1, float *y1, float *x2, float *y2) {
+    printf("Enter coordinates of point 1 (x1 y1): ");
+    scanf("%f %f", x1, y1);
+
+    printf("Enter coordinates of point 2 (x2 y2): ");
+    scanf("%f %f", x2, y2);
+}
+
+float find_distance(float x1, float y1, float x2, float y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+void output(float x1, float y1, float x2, float y2, float distance) {
+    printf("The distance between point (%.7f, %.7f) and (%.7f, %.7f) is %.4f\n", x1, y1, x2, y2, distance);
 }
