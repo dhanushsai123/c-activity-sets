61edbf8390e9decdff52e3da93250aeb98e028af
Boya shalini <boyas.bsc23@rvu.edu.in>
2024-03-01 14:19:56 +0530

    test

--- set01/problem11.c
+++ set01/problem11.c
@@ -4,31 +4,29 @@ struct _complex {
     float real;
     float imaginary;
 };
+
 typedef struct _complex Complex;
 
 Complex input_complex();
 Complex add_complex(Complex a, Complex b);
 void output(Complex a, Complex b, Complex sum);
+
 int main() {
-    Complex num1, num2, sum;
-    
-    printf("Enter the real part of the first complex number: ");
-    num1.real = input_complex().real;
-    printf("Enter the imaginary part of the first complex number: ");
-    num1.imaginary = input_complex().imaginary;
-    
-    printf("Enter the real part of the second complex number: ");
-    num2.real = input_complex().real;
-    printf("Enter the imaginary part of the second complex number: ");
-    num2.imaginary = input_complex().imaginary;
-    
-    sum = add_complex(num1, num2);
-    output(num1, num2, sum);
+    Complex num1, num2, result;
+
+    num1 = input_complex();
+    num2 = input_complex();
+
+    result = add_complex(num1, num2);
+
+    output(num1, num2, result);
+
     return 0;
 }
 
 Complex input_complex() {
     Complex num;
+    printf("Enter real and imaginary parts separated by space: ");
     scanf("%f %f", &num.real, &num.imaginary);
     return num;
 }
@@ -41,8 +39,6 @@ Complex add_complex(Complex a, Complex b) {
 }
 
 void output(Complex a, Complex b, Complex sum) {
-    printf("Sum of complex numbers:\n");
-    printf("First Complex Number: %.2f + %.2fi\n", a.real, a.imaginary);
-    printf("Second Complex Number: %.2f + %.2fi\n", b.real, b.imaginary);
-    printf("Sum: %.2f + %.2fi\n", sum.real, sum.imaginary);
+    printf("The sum of %.1f+%.1fi and %.1f+%.1fi is %.1f+%.1fi\n", 
+           a.real, a.imaginary, b.real, b.imaginary, sum.real, sum.imaginary);
 }
