5956738557a9a944f31f3f06e8c664951b14e1e5
Boya shalini <boyas.bsc23@rvu.edu.in>
2024-03-01 14:06:41 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
-int get_array_size();
-void get_array_values(int n, int a[n]);
-int find_sum_array(int n, int a[n], int sum);
-void output(int sum);
 
-int get_array_size()
-{
-    int n;
-    printf("Enter the size of array : \n");
-    scanf("%d",&n);
-    return n;
-}
-void get_array_values(int n, int a[n])
-{
-    for (int i=0; i<n; i++)
-{
-    printf("enter the values \n");
-    scanf("%d",&a[i]);
-}
-}
-int find_sum_array(int n, int a[n], int sum)
-{
-    sum=0;
-    for (int i=0; i<n; i++) 
-{
-   sum= a[i]+sum;
-}
-return sum;
-}
-void output(int sum)
-{
-    printf("the sum of arrays is %d",sum);
-}    
-int main()
-{
-    int n, sum=0;
-    n= get_array_size();
-     int a[n];
-    get_array_values(n,a);
-    sum=find_sum_array(n,a, sum);
-    output(sum);
+float input();
+float square_root(float n);
+void output(float n, float sqrroot);
+
+int main() {
+    float number, result;
+
+    number = input();
+
+    result = square_root(number);
+    output(number, result);
+
     return 0;
+}
+
+float input() {
+    float num;
+    printf("Enter number: ");
+    scanf("%f", &num);
+    return num;
+}
+
+float square_root(float n) {
+    float x = n;
+    float y = 1;
+    float epsilon = 0.000001; 
+    while (x - y > epsilon) {
+        x = (x + y) / 2;
+        y = n / x;
+    }
+
+    return x;
+}
+
+void output(float n, float sqrroot) {
+    printf("Square root of %.1f is %.1f\n", n, sqrroot);
 }
