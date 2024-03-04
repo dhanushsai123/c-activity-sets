9c72f45212aec609e4d206b918e7f3c6efec4170
Boya shalini <boyas.bsc23@rvu.edu.in>
2024-03-04 15:03:24 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 
-int input() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    return num;
-}
 
-int compare(int a, int b, int c) {
-    int largest = a;
-    
-    if (b > largest) {
-        largest = b;
-    }
-    if (c > largest) {
-        largest = c;
-    }
-    
-    return largest;
-}
-
-void output(int a, int b, int c, int largest) {
-    printf("The largest of %d, %d, and %d is %d.\n", a, b, c, largest);
-
-}
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
 
 int main() {
     int num1, num2, num3, largest;
@@ -37,4 +17,25 @@ int main() {
     output(num1, num2, num3, largest);
 
     return 0;
+}
+
+int input() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    return num;
+}
+
+int compare(int a, int b, int c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
+void output(int a, int b, int c, int largest) {
+    printf("The largest of %d, %d, and %d is %d.\n", a, b, c, largest);
 }
